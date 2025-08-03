427c3a62bfd38c42e0f8c3716d0f01cd9a026505
Godsway <gbaniba@st.ug.edu.gh>
2025-08-03 23:27:59 +0000

    Added Ticket Price Calculator app

---
+++ Ticket Price Calculator/Ticket Price Calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticket_Price_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("********DCIT 318 ASSIGNMENT********");
+            TicketPriceCalculator();
+        }
+
+        static void TicketPriceCalculator()
+        {
+                Console.Write("Enter your age: ");
+                int age;
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out age))
+                {
+                    Console.WriteLine("Invalid input! Please enter a valid number.");
+                    continue;
+                }
+
+                if (age < 0)
+                {
+                    Console.WriteLine("Age cannot be negative");
+                    continue;
+                }
+
+                break;
+            }
+
+            if (age <= 12 || age >= 65)
+                Console.WriteLine("Your ticket is GHC 7");
+            else
+                Console.WriteLine("Your ticket is GHC 10");
+
+                    
+        }
+    }
+}
